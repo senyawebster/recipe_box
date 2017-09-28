6abea17a09b77dbd4898a36ba4bd930153a42aec
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-28 09:14:32 -0700

    set up app.rb and add some views

--- app.rb
+++ app.rb
@@ -5,5 +5,16 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
 
 
 get '/' do
+  @recipes = Recipe.all
+  @tags = Tag.all
+  @ingredients = Ingredient.all
   erb(:index)
+end
+
+get '/recipe/create' do
+  erb(:create_recipe)
+end
+
+get '/category/create' do
+  erb(:create_category)
 end
