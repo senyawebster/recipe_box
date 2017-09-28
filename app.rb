e9cbee48256888e89952f91aab57678f6f487f4d
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-28 09:40:34 -0700

    add recipe-posting functionality

--- app.rb
+++ app.rb
@@ -12,9 +12,20 @@ get '/' do
 end
 
 get '/recipe/create' do
+  @recipes = Recipe.all
   erb(:create_recipe)
 end
 
+post '/recipe/create' do
+  name = params['name']
+  recipe = Recipe.create({:name => name})
+  @recipes = Recipe.all()
+  @tags = Tag.all()
+  @ingredients = Ingredient.all()
+  erb(:index)
+end
+
 get '/category/create' do
+  @tags = Tag.all
   erb(:create_category)
 end
