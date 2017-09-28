80f8918d1e5dd7e2f03fb327ab7f96d79b094286
Senya Webster <Guest@Epicodus-4E.local>
2017-09-28 16:12:38 -0700

    add some changes

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
 
+# CHANGE YOUR DATABASE SCHEMA!
 
 get '/' do
   @recipes = Recipe.all
@@ -18,11 +19,12 @@ end
 
 post '/recipe/create' do
   name = params['name']
+  instructions = ['instructions']
   rating = params['rating']
-  recipe = Recipe.create({:name => name, :rating => rating})
-  @recipes = Recipe.all()
-  @tags = Tag.all()
-  @ingredients = Ingredient.all()
+  recipe = Recipe.create({:name => name, :instructions => instructions, :rating => rating})
+  # @recipes = Recipe.all()
+  # @tags = Tag.all()
+  # @ingredients = Ingredient.all()
   erb(:index)
 end
 
@@ -36,20 +38,34 @@ end
 
 # <----- having trouble isolating categories and ingredients belonging exclusively to one recipe, and then displaying them ----->
 post 'recipe/:id' do
-  @recipe = Recipe.find(params[:id])
-  tag = params['tag']
-  ingredient = params['ingrdient']
-  # @tags = Tag.create({:food_type => food_type})
-  # @ingredients = Ingredient.create({:description => description})
-  recipe = Recipe.add({:tag_id => tag, :ingredient_id => ingredient})
+  name = params['name']
+  tag_id = params['tag_id']
+  ingredient_id = params['ingredient_id']
+  instructions = params['instructions']
+  rating = params['rating']
 
   @recipes = Recipe.all()
   @tags = Tag.all()
   @ingredients = Ingredient.all()
+
+  @recipes = Recipe.find(params[:id])
+  tag = params['tag']
+  ingredient = params['ingredient']
+  @tags = Tag.create({:food_type => food_type})
+  @ingredients = Ingredient.create({:description => description})
+  recipe = Recipe.create({:tag_id => tag, :ingredient_id => ingredient})
+
+  # @recipes = Recipe.all()
+  @tags = Tag.all()
+  @ingredients = Ingredient.all()
+
+
   erb(:recipe_card)
+  # redirect('/recipe/'.concat(@recipe))
+
 end
 # <------ End Recipe Path ----->
-get '/category/create' do
-  @tags = Tag.all
-  erb(:create_category)
-end
+# get '/category/create' do
+#   @tags = Tag.all
+#   erb(:create_category)
+# end
