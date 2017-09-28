36ef1cd68416c95d498c812b86fe8550996d767f
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-28 11:52:25 -0700

    add recipe_card.erb but still working on functionality

--- app.rb
+++ app.rb
@@ -18,13 +18,37 @@ end
 
 post '/recipe/create' do
   name = params['name']
-  recipe = Recipe.create({:name => name})
+  rating = params['rating']
+  recipe = Recipe.create({:name => name, :rating => rating})
   @recipes = Recipe.all()
   @tags = Tag.all()
   @ingredients = Ingredient.all()
   erb(:index)
 end
 
+get '/recipe/:id' do
+  @recipe = Recipe.find(params[:id])
+  @recipes = Recipe.all()
+  @tags = Tag.all()
+  @ingredients = Ingredient.all()
+  erb(:recipe_card)
+end
+
+# <----- having trouble isolating categories and ingredients belonging exclusively to one recipe, and then displaying them ----->
+post 'recipe/:id' do
+  @recipe = Recipe.find(params[:id])
+  tag = params['tag']
+  ingredient = params['ingrdient']
+  # @tags = Tag.create({:food_type => food_type})
+  # @ingredients = Ingredient.create({:description => description})
+  recipe = Recipe.add({:tag_id => tag, :ingredient_id => ingredient})
+
+  @recipes = Recipe.all()
+  @tags = Tag.all()
+  @ingredients = Ingredient.all()
+  erb(:recipe_card)
+end
+# <------ End Recipe Path ----->
 get '/category/create' do
   @tags = Tag.all
   erb(:create_category)
